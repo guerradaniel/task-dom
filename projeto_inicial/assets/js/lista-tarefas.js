0da683c5e6544d8a8a6a84ebc22a1365871f965c
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-03-18 17:09:04 -0300

    Criado comportamento ao clicar em botao.

--- projeto_inicial/assets/js/lista-tarefas.js
+++ projeto_inicial/assets/js/lista-tarefas.js
@@ -33,10 +33,16 @@ const BotaoConclui = () => {
 
     botaoConclui.classList.add('check-button')
     botaoConclui.innerText = 'concluir'
-    botaoConclui.addEventListener('click', () => {
-        console.log('fui clicado')
-    })
+    botaoConclui.addEventListener('click', concluirTarefa)
 
     return botaoConclui
 }
 
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+
+    const tarefaCompleta = botaoConclui.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+}

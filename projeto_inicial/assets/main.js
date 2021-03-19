8c8bfbe89d44ebccab27ab65f4abc7cd8c92f227
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-03-19 15:48:41 -0300

    Criado moment.js ao projeto e campos de data para inputar.

--- projeto_inicial/assets/main.js
+++ projeto_inicial/assets/main.js
@@ -2,14 +2,16 @@ import BotaoConclui from '../components/conclui-tarefa.js'
 import BotaoDeleta from '../components/deleta-tarefa.js'
 
 
-const criarTarefa = (evento) => {
-
+const handleNovoItem = (evento) => {
     evento.preventDefault()
-
     const list = document.querySelector('[data-list]')
-
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
+
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+
+    const dataFormat = data.format('DD/MM/YYYY')
 
     const divErro = document.querySelector('[data-erro]')
     const erro = `<p class="mensagem-erro">Campo vazio!</p>`
@@ -21,21 +23,28 @@ const criarTarefa = (evento) => {
         divErro.innerHTML = ''
     }
 
+    list.appendChild(tarefa)
+    input.value = " "
+
+}
+
+const criarTarefa = () => {
+
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
 
-    const conteudo = `<p class="content">${valor}</p>`
+    const conteudo = `<p class="content">${dataFormat} * ${valor}</p>`
 
     tarefa.innerHTML = conteudo
 
     tarefa.appendChild(BotaoConclui())
     tarefa.appendChild(BotaoDeleta())
 
-    list.appendChild(tarefa)
-    input.value = " "
+    return tarefa
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handleNovoItem)
 
 
+// parei em ultima video-aula JS na web
